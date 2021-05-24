3eaa4f39a5f04c19fba52b6e533b4866ee3ae3f9
Wojciech Cendrzak <23259795+WojciechCendrzak@users.noreply.github.com>
2021-05-24 20:05:46 +0200

    staged test

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,5 +2,5 @@ module.exports = {
   // Run type-check on changes to TypeScript files
   '**/*.ts?(x)': () => 'yarn tsc',
   // Run ESLint on changes to JavaScript/TypeScript files
-  '**/*.(ts|js)?(x)': (filenames) => `yarn lint ${filenames.join(' ')}`,
+  '**/*.(ts)?(x)': (filenames) => `yarn lint ${filenames.join(' ')}`,
 };
