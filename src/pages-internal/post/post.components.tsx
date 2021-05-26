11f3493df9bf35122dcb5ec68df56fd638811a98
Wojciech Cendrzak <23259795+WojciechCendrzak@users.noreply.github.com>
2021-05-26 23:43:28 +0200

    code snipfets fixes

--- src/pages-internal/post/post.components.tsx
+++ src/pages-internal/post/post.components.tsx
@@ -1,16 +1,31 @@
 import React from 'react';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { Components } from 'react-markdown/src/ast-to-react';
+import styled from 'styled-components';
 
 export const markDownComponents: Components = {
   code({ inline, className, children, ...props }) {
     const match = /language-(\w+)/.exec(className || '');
     return !inline && match ? (
-      <SyntaxHighlighter language={match[1]} PreTag="div" {...props}>
+      <Code language={match[1]} PreTag="div" {...props}>
         {String(children).replace(/\n$/, '')}
-      </SyntaxHighlighter>
+      </Code>
     ) : (
       <code className={className} {...props} />
     );
   },
 };
+
+const Code = styled(SyntaxHighlighter)`
+  background: #f1f1f1 !important;
+  border-radius: 8px;
+  text-shadow: none !important;
+
+  code {
+    text-shadow: none !important;
+  }
+
+  .token {
+    background: transparent !important;
+  }
+`;
